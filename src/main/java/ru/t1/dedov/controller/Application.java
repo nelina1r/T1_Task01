edcaad54a6071bada2533ccb34738a263a58d832
Alexander Dedov <78604652+nelina1r@users.noreply.github.com>
2022-04-28 00:30:34 +0300

    deleted useless try-catch in method main

--- src/main/java/ru/t1/dedov/controller/Application.java
+++ src/main/java/ru/t1/dedov/controller/Application.java
@@ -11,16 +11,12 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        List<Department> departmentList = new ArrayList<>();
+        List<Department> departmentList;
         try{
             departmentList = FileReaderService.readFromFileToList(args[0]);
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("File name not entered");
-        }
-        try{
             FileWriterService.writeInFile(CalculatorService.calculateEmployeeTransfers(departmentList), args[1]);
         }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("\nOutput file name not entered");
+            System.out.println("File name not entered");
         }
     }
 }

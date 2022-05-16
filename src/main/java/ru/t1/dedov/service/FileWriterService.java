0c608e80ce6689f1f563b50d99d4c2040e6b9dc0
Alexander Dedov <78604652+nelina1r@users.noreply.github.com>
2022-05-16 14:29:35 +0300

    fixed writeLineInFile method

--- src/main/java/ru/t1/dedov/service/FileWriterService.java
+++ src/main/java/ru/t1/dedov/service/FileWriterService.java
@@ -6,21 +6,11 @@ public class FileWriterService {
 
     public static String OUTPUT_FILE_NAME;
 
-    public static void writeLineInFile(String text){
-        File file = new File(OUTPUT_FILE_NAME);
-        FileWriter fr = null;
-        try {
-            fr = new FileWriter(file,true);
-            fr.write(text);
-
-        } catch (IOException e) {
-            System.out.println("Error occurred during writing in file: " + OUTPUT_FILE_NAME);
-        }finally{
-            try {
-                fr.close();
-            } catch (IOException e) {
-                System.out.println("Error occurred during closing the file: " + OUTPUT_FILE_NAME);
-            }
+    public static void writeLineInFile(String text) {
+        try(FileWriter fileWriter = new FileWriter(OUTPUT_FILE_NAME, true)){
+            fileWriter.write(text);
+        }catch (IOException e){
+            System.out.println("Error occurred during writing the file: " + OUTPUT_FILE_NAME);
         }
     }
 }

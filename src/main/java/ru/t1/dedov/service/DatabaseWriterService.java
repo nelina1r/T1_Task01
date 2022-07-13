965552609b4d93666b4e3e1620d1867235f2d97c
Alexnder Dedov <dedov.al@bk.ru>
2022-07-13 17:02:15 +0400

    fixed insertData method

--- src/main/java/ru/t1/dedov/service/DatabaseWriterService.java
+++ src/main/java/ru/t1/dedov/service/DatabaseWriterService.java
@@ -11,29 +11,35 @@ public class DatabaseWriterService {
     private final String DATABASE_URL = "jdbc:postgresql://localhost:5432/task1_db";
     private final String USER = "postgres";
     private final String PASSWORD = "postgres";
+    private final String DATABASE_DRIVER = "org.postgresql.Driver";
 
     public void insertData(Map<String, Department> departmentMap) throws SQLException, ClassNotFoundException {
-        Connection connection = null;
-        Statement statement = null;
-
-        Class.forName("org.postgresql.Driver");
-
-        connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+        Connection connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);;
+        Class.forName(DATABASE_DRIVER);
         connection.setAutoCommit(false);
-        statement = connection.createStatement();
+        //
+        String sql1 = "insert into department (name) values (?)";
+        String sql2 = "insert into employee (name, salary, department_id) values (?,?,?)";
+        //
+        String generatedColumns[] = {"ID"};
+        PreparedStatement ps1 = connection.prepareStatement(sql1, generatedColumns);
+        PreparedStatement ps2 = connection.prepareStatement(sql2);
         for(Map.Entry<String, Department> entry : departmentMap.entrySet()) {
             try {
-                String SQL1 = "insert into department (name) values (" +  "'" + entry.getKey() + "'" + ")";
-                statement.executeUpdate(SQL1);
-                String SQL2 = "select id from department where name = '" + entry.getKey() + "'";
-                ResultSet resultSet = statement.executeQuery(SQL2);
+                ps1.setString(1, entry.getKey());
+                ps1.executeUpdate();
+                //
                 Long id = 0L;
-                while(resultSet.next())
-                    id = resultSet.getLong("id");
+                ResultSet rs = ps1.getGeneratedKeys();
+                if (rs.next()) {
+                    id = rs.getLong(1);
+                }
+                //
                 for(Employee emp : entry.getValue().getEmployeeList()){
-                    String SQL3 =
-                            "insert into employee (name, salary, department_id) values (" + "'" +emp.getName() + "'" + ", " + emp.getSalary() + "," + id + ")";
-                    statement.executeUpdate(SQL3);
+                    ps2.setString(1, emp.getName());
+                    ps2.setBigDecimal(2, emp.getSalary());
+                    ps2.setLong(3, id);
+                    ps2.executeUpdate();
                 }
                 connection.commit();
             } catch (SQLException e) {
